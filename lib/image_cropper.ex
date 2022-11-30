82b467068343c9fa4bd0b363fef57a762d8ce1ae
Cristiano Carvahlo <cristiano.carvalho23@icloud.com>
2022-11-30 20:21:12 -0300

    chore: add crop_and_grayscale func

--- lib/image_cropper.ex
+++ lib/image_cropper.ex
@@ -11,4 +11,13 @@ defmodule ImageCropper do
 
   @spec add(integer(), integer()) :: integer()
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+
+  @spec crop_and_grayscale(
+          binary(),
+          non_neg_integer(),
+          non_neg_integer(),
+          non_neg_integer(),
+          non_neg_integer()
+        ) :: binary()
+  def crop_and_grayscale(image, x, y, width, height), do: :erlang.nif_error(:nif_not_loaded)
 end
