5bca8e086e49f7a38d6057a390225dfd8845a450
Cristiano Carvahlo <cristiano.carvalho23@icloud.com>
2022-12-12 21:39:17 -0300

    chore: hook up grayscale and crop

--- lib/image_cropper_web/controllers/image_controller.ex
+++ lib/image_cropper_web/controllers/image_controller.ex
@@ -10,6 +10,21 @@ defmodule ImageCropperWeb.ImageController do
 
   def create(conn, %{"image" => image_params}) do
     changeset = Image.changeset(%Image{}, image_params)
-    render(conn, "new.html", changeset: changeset)
+
+    if changeset.valid? do
+      %{file: %{path: path}} = changeset.changes
+      %{x: x, y: y, width: width, height: height} = changeset.changes
+
+      image_buffer = File.read!(path)
+
+      image_b64 =
+        image_buffer
+        |> ImageCropper.crop_and_grayscale(x, y, width, height)
+        |> Base.encode64()
+
+      render(conn, "show.html", image_src: "data:image/png;base64,#{image_b64}")
+    else
+      render(conn, "new.html", changeset: changeset)
+    end
   end
 end
